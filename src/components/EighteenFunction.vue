15ab7b29ccfab6d97d21743e7f30d702cecc10a6
Cortina17 <diego.cortinagarcia@gmail.com>
2022-02-10 12:55:34 +0100

    added route to functions

---
+++ src/components/EighteenFunction.vue
@@ -0,0 +1,30 @@
+<template>
+	<v-container>
+		<div id="app">
+			{{ message }}
+		</div>
+		<v-footer dark padless>
+			<v-card class="flex" flat tile>
+				<v-card-title class="teal">
+					<strong class="subheading"
+						>Get connected with us on social networks!</strong
+					>
+					<v-spacer></v-spacer>
+				</v-card-title>
+
+				<v-card-text class="py-2 white--text text-center">
+					{{ new Date().getFullYear() }} — <strong>Vuetify</strong>
+				</v-card-text>
+			</v-card>
+		</v-footer>
+	</v-container>
+</template>
+
+<script>
+export default {
+	name: "app",
+	data: () => ({
+		message: "APP to tell if someone is adult after write their age",
+	}),
+};
+</script>
---
+++ src/views/Functions.vue
@@ -0,0 +1,15 @@
+<template>
+	<eighteen-function />
+</template>
+
+<script>
+import EighteenFunction from "../components/EighteenFunction.vue";
+
+export default {
+	name: "Functions",
+
+	components: {
+		EighteenFunction,
+	},
+};
+</script>
